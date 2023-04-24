6a1cf37dee937626aabf2f129fe2cbc005e40123
Hin <tonghohin77@gmail.com>
2023-04-23 21:08:10 -0300

    Fixed test

--- src/__tests__/aggregateData.test.ts
+++ src/__tests__/aggregateData.test.ts
@@ -2,15 +2,21 @@ import aggregateData from "@/utils/aggregateData";
 import Data from "@/interfaces/Data";
 
 const MOCK_DATA: Data[] = [
-    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": "Earthquake", "Risk Rating": 0.5, Year: "2030" },
-    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": "Earthquake", "Risk Rating": 0.5, Year: "2040" },
-    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": "Earthquake", "Risk Rating": 1, Year: "2050" },
-    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": "Earthquake", "Risk Rating": 0, Year: "2060" },
-    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": "Earthquake", "Risk Rating": 0, Year: "2070" }
+    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": { Earthquake: 0.3 }, "Risk Rating": 0.5, Year: "2030" },
+    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": { Earthquake: 0.3 }, "Risk Rating": 0.5, Year: "2040" },
+    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": { Earthquake: 0.3 }, "Risk Rating": 1, Year: "2050" },
+    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": { Earthquake: 0.3 }, "Risk Rating": 0.2, Year: "2060" },
+    { "Asset Name": "This is an asset", "Business Category": "This is a business", Lat: 90, Long: 32, "Risk Factors": { Earthquake: 0.3 }, "Risk Rating": 0.2, Year: "2070" }
 ];
 
 describe("testing aggregateData function", () => {
     test("correct aggregation", () => {
-        expect(aggregateData(MOCK_DATA, "This is an asset", "Asset Name")).toEqual([0.5, 0.5, 1, 0, 0]);
+        expect(aggregateData(MOCK_DATA, "This is an asset", "Asset Name")).toEqual([
+            { x: 1, y: 0.5, highestRisk: { risk: "Earthquake", rating: 0.3 } },
+            { x: 1, y: 0.5, highestRisk: { risk: "Earthquake", rating: 0.3 } },
+            { x: 1, y: 1, highestRisk: { risk: "Earthquake", rating: 0.3 } },
+            { x: 1, y: 0.2, highestRisk: { risk: "Earthquake", rating: 0.3 } },
+            { x: 1, y: 0.2, highestRisk: { risk: "Earthquake", rating: 0.3 } }
+        ]);
     });
 });
