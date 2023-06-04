0072928a0d354717c8c6452b19ad29c5e00c91e5
Hin <tonghohin77@gmail.com>
2023-06-04 16:05:34 -0300

    Google index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 
 export const metadata = {
     title: "Risk-Viz",
-    description: "Risk visualization application designed and developed by Hin Tong, for Riskthinking.ai."
+    description: "Risk visualization application designed and developed by Hin Tong, for Riskthinking.ai.",
+    other: { "google-site-verification": "2LN9DEDc4V6MxSyl_z25KsXjm-D39d8SWmubhBCW9Vc" }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
