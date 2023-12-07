b1190309c6a018eb3c4da570c5d492c01290b490
Hin <tonghohin77@gmail.com>
2023-12-06 23:23:49 -0400

    Update package.json and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true
-    },
     env: {
-        API_KEY: process.env.API_KEY,
-        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
+        API_KEY: process.env.GOOGLE_MAPS_API_KEY
     }
 };
 
